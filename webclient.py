a19ff7e4d5cd1f4615d16458d54d13830e4509bf
da4lin <ryo.wasenet0821@gmail.com>
2019-10-25 18:27:59 +0900

    websocket_pers

--- webclient.py
+++ webclient.py
@@ -1,46 +1,46 @@
-import sys
 import websocket
-import time
 from threading import Thread
+import time
+import sys
+
 
 def on_message(ws, message):
-    print("debug: called on_message")
     print(message)
 
+
 def on_error(ws, error):
-    print("debug: called on_error")
     print(error)
+
 
 def on_close(ws):
     print("### closed ###")
 
+
 def on_open(ws):
     def run(*args):
-        print("debug: websocket is opened")
+        for i in range(3):
+            # send the message, then wait
+            # so thread doesn't exit and socket
+            # isn't closed
+            ws.send("Hello %d" % i)
+            time.sleep(1)
 
-        while(True):
-            line = sys.stdin.readline()
-            if line != "":
-                print("debug: sending value is " + line)
-                ws.send(line)
+        time.sleep(1)
+        ws.close()
+        print("Thread terminating...")
 
-    Thread.start_new_thread(run, ())
+    Thread(target=run).start()
 
 
 if __name__ == "__main__":
-
-    param = sys.argv
-
-    url = "localhost"
-
-    if len(param) == 2:
-        url = param[1]
-        print("debug: param[1] is " + param[1])
-
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(url,
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
+    if len(sys.argv) < 2:
+        host = "localhost:3000"
+    else:
+        host = sys.argv[1]
+    ws = websocket.WebSocketApp(host,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
     ws.on_open = on_open
     ws.run_forever()
